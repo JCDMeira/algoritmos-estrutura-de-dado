9460ee0592e9f0e0f095d7f7cc18193fe22fe4c9
Jean Meira <jean.meira10@hotmail.com>
2022-04-25 19:53:38 -0300

    feat: add how to create new arrays

--- js/array.js
+++ js/array.js
@@ -1,5 +1,6 @@
 // * Aula 01 de arrays
 
+console.log("------------- * Aula 01 de arrays----------------");
 /*
   - Em js se pode ter array de vários tipos, mas no geral se fala de uma estrutura com um tipo de dados
 */
@@ -23,3 +24,40 @@ avgTemp[4] = 25.5;
 console.log("array de médias de temperatura", avgTemp);
 console.log("temperatura de janeiro", avgTemp[0]);
 console.log("temperatura de fevereiro", avgTemp[1]);
+
+console.log("------------- * Aula 02 de arrays----------------");
+
+/*
+  _ O js não obriga o tamanho do array ser declarado junto ao inicializar.
+  _ isso é dinâmico, ao contrário de outras linguagens, e há métodos que podem alterar o tamanho do array.
+  _ Ao inserir ou remover elementos o tamanho acompanha.
+*/
+
+const daysOfWeek = new Array();
+const daysOfWeekWithLenght = new Array(7);
+const daysOfWeekWithDays = new Array(
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday"
+);
+
+daysOfWeek[0] = "Sunday";
+
+console.log("daysOfWeek primeira posição : ", daysOfWeek[0]);
+console.log("daysOfWeek lenght : ", daysOfWeek.length);
+console.log(
+  "daysOfWeekWithLenght : ",
+  daysOfWeekWithLenght,
+  "daysOfWeekWithLenght length : ",
+  daysOfWeekWithLenght.length
+);
+console.log(
+  "daysOfWeekWithDays : ",
+  daysOfWeekWithLenght,
+  "length : ",
+  daysOfWeekWithDays.length
+);
