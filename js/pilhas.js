154244b16e4e6c680b5731fe4fdb0289b3bc50e9
Jean Meira <jean.meira10@hotmail.com>
2022-04-27 19:28:53 -0300

    feat: implements stacks methods

--- js/pilhas.js
+++ js/pilhas.js
@@ -9,29 +9,36 @@ function stack() {
 
   this.push = function (element) {
     //@ Adiciona um novo item ao topo da pilha
+    items.push(element);
   };
 
   this.pop = function () {
     //@ remove um item do topo da pilha
+    return items.pop();
   };
 
   this.peek = function () {
     //@ devolve o elemento que está no topo da pilha
+    return items[items.length - 1];
   };
 
   this.isEmpty = function () {
     //@ informa se a pilha está vazia ou não
+    return items.length === 0;
   };
 
   this.clear = function () {
     //@ Limpa a pilha
+    items = [];
   };
 
   this.size = function () {
     //@ informa o tamanho da pilha
+    return items.length;
   };
 
   this.isEmpty = function () {
     //@ imprime a pilha no console
+    console.log(items.toString());
   };
 }
