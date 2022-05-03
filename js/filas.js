b6a7980614052c07ccca5da59bde1a02e8256aa1
Jean Meira <jean.meira10@hotmail.com>
2022-05-03 20:02:26 -0300

    feat: add queue

---
+++ js/filas.js
@@ -0,0 +1,27 @@
+function Queue() {
+  let items = [];
+
+  this.enqueu = () => {
+    //@ adiciona um novo item
+  };
+
+  this.dequeu = () => {
+    //@ remove um item
+  };
+
+  this.front = () => {
+    //@ retorna o primeiro elemento da fila
+  };
+
+  this.isEmpty = () => {
+    //@ verifica se a fila está vazia ou não
+  };
+
+  this.size = () => {
+    //@ retorna o tamanho da fila
+  };
+
+  this.print = () => {
+    //@ imprimir a fila no console
+  };
+}
