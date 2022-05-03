a97e048d980bf958cc99100af0bdf63bd9ed4003
Jean Meira <jean.meira10@hotmail.com>
2022-05-02 21:31:14 -0300

    feat: add decimal to binary converter with stack

---
+++ js/ex1Pilha.js
@@ -0,0 +1,19 @@
+function dec28In(decNumber) {
+  let restStack = [];
+  let rest;
+  let binaryString = "";
+
+  while (decNumber > 0) {
+    rest = Math.floor(decNumber % 2);
+    restStack.push(rest);
+    decNumber = Math.floor(decNumber / 2);
+  }
+
+  while (restStack.length > 0) {
+    binaryString += restStack.pop().toString();
+  }
+
+  return binaryString;
+}
+
+console.log(dec28In(2));
