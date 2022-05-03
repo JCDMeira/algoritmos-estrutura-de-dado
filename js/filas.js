22ee00922d4c715fe2efa1d7cbc84289e4694840
Jean Meira <jean.meira10@hotmail.com>
2022-05-03 20:23:11 -0300

    feat: use Queue

--- js/filas.js
+++ js/filas.js
@@ -3,26 +3,32 @@ function Queue() {
 
   this.enqueue = (element) => {
     //@ adiciona um novo item
+    console.log("adicionou:", element);
+
     items.push(element);
   };
 
   this.dequeue = () => {
     //@ remove um item
-    return items.unshift();
+    console.log("removeu:", items[0]);
+    return items.shift();
   };
 
   this.front = () => {
     //@ retorna o primeiro elemento da fila
+    console.log("O primeiro elemento é:", items[0]);
     return items[0];
   };
 
   this.isEmpty = () => {
     //@ verifica se a fila está vazia ou não
+    console.log(items.length === 0 ? "está vazio" : "não está vazio");
     return items.length === 0;
   };
 
   this.size = () => {
     //@ retorna o tamanho da fila
+    console.log("O tamanho da fila é:", items.length);
     return items.length;
   };
 
@@ -31,3 +37,28 @@ function Queue() {
     console.log(items.toString());
   };
 }
+
+const fila = new Queue();
+
+fila.enqueue("Calos");
+fila.enqueue("Ana");
+fila.enqueue("Lucas");
+
+fila.print();
+
+fila.dequeue();
+fila.print();
+
+fila.dequeue();
+fila.print();
+
+fila.isEmpty();
+fila.front();
+
+fila.enqueue("Gisele");
+fila.size();
+
+fila.dequeue();
+fila.dequeue();
+fila.isEmpty();
+fila.size();
