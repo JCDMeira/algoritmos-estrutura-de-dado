b40f07fb48e1fc1e949ed84f0cb4ef69b51e88ab
Jean Meira <jean.meira10@hotmail.com>
2022-05-04 20:41:43 -0300

    feat: implements hot potato game

--- js/filaEx1.js
+++ js/filaEx1.js
@@ -2,43 +2,31 @@ function Queue() {
   let items = [];
 
   this.enqueue = (element) => {
-    //@ adiciona um novo item
-    console.log("adicionou:", element);
-
     items.push(element);
   };
 
   this.dequeue = () => {
-    //@ remove um item
-    console.log("removeu:", items[0]);
     return items.shift();
   };
 
   this.front = () => {
-    //@ retorna o primeiro elemento da fila
-    console.log("O primeiro elemento é:", items[0]);
     return items[0];
   };
 
   this.isEmpty = () => {
-    //@ verifica se a fila está vazia ou não
-    console.log(items.length === 0 ? "está vazio" : "não está vazio");
     return items.length === 0;
   };
 
   this.size = () => {
-    //@ retorna o tamanho da fila
-    console.log("O tamanho da fila é:", items.length);
     return items.length;
   };
 
   this.print = () => {
-    //@ imprimir a fila no console
     console.log(items.toString());
   };
 }
 
-function hotPotato(name, num) {
+function hotPotato(nameList, num) {
   const queue = new Queue();
 
   for (let i = 0; i < nameList.length; i++) {
@@ -52,8 +40,12 @@ function hotPotato(name, num) {
       queue.enqueue(queue.dequeue());
     }
     eliminated = queue.dequeue();
-    console.log(eliminated + "was eliminated from Hot Potato Game");
+    console.log(eliminated + " was eliminated from Hot Potato Game");
   }
 
   return queue.dequeue();
 }
+
+let names = ["João", "José", "Maria", "Ana", "Lucas"];
+let winner = hotPotato(names, 7);
+console.log("The winner is: " + winner);
