95519683fe9d1685a8a07aaaabea336a39402bbc
Jean Meira <jean.meira10@hotmail.com>
2022-05-07 20:29:25 -0300

    feat: implements method insert

--- js/linkedLists.js
+++ js/linkedLists.js
@@ -30,6 +30,29 @@ function LikedList() {
 
   this.insert = (position, element) => {
     //@ adiciona um elemento em uma posição especifica
+    if (position >= 0 && position <= length) {
+      let node = new Node(element),
+        current = head,
+        previous,
+        index = 0;
+
+      if (position === 0) {
+        node.next = current;
+        head = node;
+      } else {
+        while (index++ < position) {
+          previous = current;
+          current = current.next;
+        }
+
+        node.next = current;
+        previous.next = node;
+      }
+      length++;
+      return true;
+    } else {
+      return false;
+    }
   };
 
   this.removeAt = (position) => {
