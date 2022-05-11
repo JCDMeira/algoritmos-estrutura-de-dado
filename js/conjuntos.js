8d63937a8f187f98ad9c5a5c99504a9f13c1ae57
Jean Meira <jean.meira10@hotmail.com>
2022-05-10 21:07:10 -0300

    feat: implements sets structure

---
+++ js/conjuntos.js
@@ -0,0 +1,29 @@
+//* sets
+
+function Set() {
+  let items = [];
+
+  this.add = (value) => {
+    //@ adiciona um novo item ao conjunto
+  };
+
+  this.delete = (value) => {
+    //@ remove um item ao conjunto
+  };
+
+  this.has = (value) => {
+    //@ devolve se o valor existe ou não
+  };
+
+  this.clear = (value) => {
+    //@ remove todos os itens do conjunto
+  };
+
+  this.size = (value) => {
+    //@ retorna o tamanho do conjunto
+  };
+
+  this.values = (value) => {
+    //@ retorna um array com todos os valores do conjunto
+  };
+}
