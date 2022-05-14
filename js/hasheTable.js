aaafecedb5220df3f664c4486b9554d7e21327d9
Jean Meira <jean.meira10@hotmail.com>
2022-05-14 16:24:04 -0300

    feat: test hashTable methods

--- js/hasheTable.js
+++ js/hasheTable.js
@@ -18,13 +18,27 @@ function HashTable() {
     return table[losoloseHashCode(key)];
   };
 
-  let losoloseHashCode = (key) => {
+  const losoloseHashCode = (key) => {
     //@ retorna hash (valor numérico)
     let hash = 0;
 
     for (let i = 0; i < key.length; i++) {
       hash += key.charCodeAt(i);
+      // console.log(key[i] + " : " + key.charCodeAt(i));
     }
+    // console.log(hash);
     return hash % 37;
   };
 }
+
+const hash = new HashTable();
+
+hash.put("Gandalf", "gandalf@email.com");
+hash.put("John", "john@email.com");
+hash.put("Tyrion", "tyrion@email.com");
+
+console.log(hash.get("Gandalf"));
+console.log(hash.get("José"));
+
+hash.remove("Gandalf");
+console.log(hash.get("Gandalf"));
