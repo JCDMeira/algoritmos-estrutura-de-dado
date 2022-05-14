87924befa8a85241b587f6128a0ebd003129a8ac
Jean Meira <jean.meira10@hotmail.com>
2022-05-14 16:10:21 -0300

    feat: add hashTable

---
+++ js/hasheTable.js
@@ -0,0 +1,19 @@
+function HashTable() {
+  let table = [];
+
+  this.put() = (key, value)=>{
+    //@ insere elemento
+  }
+
+  this.remove() = (key)=>{
+    //@ remove elemento
+  }
+
+  this.get() = (key)=>{
+    //@ retorna um elemento
+  }
+
+  this.losoloseHashCode() = (key)=>{
+    //@ retorna hash (valor numérico)
+  }
+}
