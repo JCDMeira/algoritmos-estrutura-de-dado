5b86f92987782c1609ce186e96d81a4e550ce81e
Jean Meira <jean.meira10@hotmail.com>
2022-05-13 22:48:29 -0300

    feat: add Dictionary

---
+++ js/dicionarios-e-hashes.js
@@ -0,0 +1,39 @@
+function Dictionary() {
+  const items = {};
+
+  this.set = (key, value) => {
+    //@ adiciona um novo item ao dicionário
+  };
+
+  this.delete = (key) => {
+    //@ remove o valor do dicionário usando a chave
+  };
+
+  this.has = (key) => {
+    //@ verifica se a chave existe e retorna um booleano
+  };
+
+  this.has = (key) => {
+    //@ verifica se a chave existe e retorna um booleano
+  };
+
+  this.get = (key) => {
+    //@ devolve um valor específico a partir da chave
+  };
+
+  this.clear = () => {
+    //@ remove todos os itens do dicionário
+  };
+
+  this.size = () => {
+    //@ retorna a quantidade de elementos contidos no dicionário
+  };
+
+  this.keys = () => {
+    //@ devolve um array com todas as chaves do dicionário
+  };
+
+  this.values = () => {
+    //@ devolve um array com todos os valores do dicionário
+  };
+}
