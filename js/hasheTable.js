dc34f3f0049d3eb8a3dc9086e2c23480f11a364b
Jean Meira <jean.meira10@hotmail.com>
2022-05-14 16:16:12 -0300

    feat: implements  hashTable methods

--- js/hasheTable.js
+++ js/hasheTable.js
@@ -1,19 +1,30 @@
 function HashTable() {
   let table = [];
 
-  this.put() = (key, value)=>{
+  this.put = (key, value) => {
     //@ insere elemento
-  }
+    let position = losoloseHashCode(key);
+    console.log(position + " " + key);
+    table[position] = value;
+  };
 
-  this.remove() = (key)=>{
+  this.remove = (key) => {
     //@ remove elemento
-  }
+    table[losoloseHashCode(key)] = undefined;
+  };
 
-  this.get() = (key)=>{
+  this.get = (key) => {
     //@ retorna um elemento
-  }
+    return table[losoloseHashCode(key)];
+  };
 
-  this.losoloseHashCode() = (key)=>{
+  let losoloseHashCode = (key) => {
     //@ retorna hash (valor numérico)
-  }
+    let hash = 0;
+
+    for (let i = 0; i < key.length; i++) {
+      hash += key.charCodeAt(i);
+    }
+    return hash % 37;
+  };
 }
