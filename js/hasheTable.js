e465964d74e68031ca440d5f369bdaed1dacaa7c
Jean Meira <jean.meira10@hotmail.com>
2022-05-15 15:15:31 -0300

    feat: add collision handling p2

--- js/hasheTable.js
+++ js/hasheTable.js
@@ -1,12 +1,167 @@
+function LinkedList() {
+  let Node = function (element) {
+    this.element = element;
+    this.next = null;
+  };
+
+  let length = 0;
+  let head = null;
+
+  this.append = (element) => {
+    //@ adiciona um elemento no final da lista
+    let node = new Node(element),
+      current;
+
+    if (head === null) {
+      head = node;
+    } else {
+      current = head;
+      while (current.next) {
+        current = current.next;
+      }
+
+      current.next = node;
+    }
+
+    length++;
+  };
+
+  this.insert = (position, element) => {
+    //@ adiciona um elemento em uma posição especifica
+    if (position >= 0 && position <= length) {
+      let node = new Node(element),
+        current = head,
+        previous,
+        index = 0;
+
+      if (position === 0) {
+        node.next = current;
+        head = node;
+      } else {
+        while (index++ < position) {
+          previous = current;
+          current = current.next;
+        }
+
+        node.next = current;
+        previous.next = node;
+      }
+      length++;
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  this.removeAt = (position) => {
+    //@ remove um elemento de uma posição especifica
+    if (position > -1 && position < length) {
+      let current = head,
+        previous,
+        index = 0;
+
+      if (position === 0) {
+        head = current.next;
+      } else {
+        while (index++ < position) {
+          previous = current;
+          current = current.next;
+        }
+        previous.next = current.next;
+      }
+
+      length--;
+
+      return current.element;
+    } else {
+      return null;
+    }
+  };
+
+  this.remove = (element) => {
+    //@ remove o elemento element (recebido por parâmetro)
+    let index = this.indexOf(element);
+    return this.removeAt(index);
+  };
+
+  this.indexOf = (element) => {
+    //@ retorna a posição do elemento element
+    let current = head,
+      index = 0;
+
+    while (current) {
+      if (element === current.element) {
+        return index;
+      }
+      index++;
+      current = current.next;
+    }
+    return -1;
+  };
+
+  this.isEmpty = (element) => {
+    //@ retorna se a lista está vazia ou não
+    return length === 0;
+  };
+
+  this.size = (element) => {
+    //@ retorna o tamanho da instância
+    return length;
+  };
+
+  this.getHead = () => {
+    return head;
+  };
+
+  this.toString = (element) => {
+    //@ converte em string
+    let current = head;
+    let string = "";
+
+    while (current) {
+      string += current.element + " ";
+      current = current.next;
+    }
+
+    return string;
+  };
+
+  this.print = (element) => {
+    //@ imprime no console
+    console.log(this.toString());
+  };
+}
+
 function HashTable() {
   let table = [];
+
+  const ValuePair = (key, value) => {
+    this.key = key;
+    this.value = value;
+    this.toString = () => `[ ${this.key} - ${this.value}]`;
+  };
+
+  this.set = (key, value) => {
+    //@ adiciona um novo item ao dicionário
+    items[key] = value;
+  };
 
   this.put = (key, value) => {
     //@ insere elemento
     let position = losoloseHashCode(key);
-    // console.log(position + " " + key);
-    table[position] = value;
+
+    if (table[position] === undefined) {
+      table[position] = new LinkedList();
+    }
+    table[position].append(new ValuePair(key, value));
   };
+
+  // this.put = (key, value) => {
+  //   //@ insere elemento
+  //   let position = losoloseHashCode(key);
+  //   // console.log(position + " " + key);
+  //   table[position] = value;
+  // };
 
   this.remove = (key) => {
     //@ remove elemento
