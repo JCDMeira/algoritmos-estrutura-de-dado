ec03bd242e86954cab1d64c2519ca8e89a108a6e
Jean Meira <jean.meira10@hotmail.com>
2022-05-22 18:19:16 -0300

    feat: add mergeSort

--- js/ordenacao.js
+++ js/ordenacao.js
@@ -1,5 +1,5 @@
 function ArrayList() {
-  const array = [];
+  let array = [];
 
   this.insert = (item) => {
     array.push(item);
@@ -38,7 +38,7 @@ function ArrayList() {
   this.insertionSort = () => {
     let j, temp;
 
-    for (let i = 0; i < array.length; i++) {
+    for (let i = 1; i < array.length; i++) {
       j = i;
       temp = array[i];
 
@@ -48,6 +48,48 @@ function ArrayList() {
       }
       array[j] = temp;
     }
+  };
+
+  this.mergeSort = () => {
+    array = mergeSortRec(array);
+  };
+
+  const mergeSortRec = (array) => {
+    let length = array.length;
+
+    if (length === 1) {
+      return array;
+    }
+
+    let mid = Math.floor(length / 2),
+      left = array.slice(0, mid),
+      right = array.slice(mid, length);
+
+    return merge(mergeSortRec(left), mergeSortRec(right));
+  };
+
+  const merge = (left, right) => {
+    const result = [];
+    let il = 0,
+      ir = 0;
+
+    while (il < left.length && ir < right.length) {
+      if (left[il] < right[ir]) {
+        result.push(left[il++]);
+      } else {
+        result.push(right[ir++]);
+      }
+    }
+
+    while (il < left.length) {
+      result.push(left[il++]);
+    }
+
+    while (ir < right.length) {
+      result.push(right[ir++]);
+    }
+
+    return result;
   };
 
   const swap = (array, index1, index2) => {
@@ -62,7 +104,7 @@ const al = new ArrayList();
 
 al.insert(5);
 al.insert(4);
-al.insert(5);
+al.insert(3);
 al.insert(2);
 al.insert(1);
 console.log(al.toString());
@@ -71,6 +113,8 @@ console.log(al.toString());
 
 // al.selectionSort();
 
-al.insertionSort();
+// al.insertionSort();
+
+al.mergeSort();
 
 console.log(al.toString());
